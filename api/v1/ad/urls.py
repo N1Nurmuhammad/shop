724f252422b51471ea00f40d306946997384fb84
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-05-30 16:41:24 +0500

    cors

--- api/v1/ad/urls.py
+++ api/v1/ad/urls.py
@@ -4,13 +4,14 @@ from api.v1.ad.views import *
 app_name = 'ad'
 
 urlpatterns = [
-    path('create-ad/', create_ad, name='create-ad'),
     path('update-ad/<int:pk>', update_ad, name='update-ad'),
     path('delete-ad/<int:pk>', delete_ad, name='delete-ad'),
     path('get-ad/<int:pk>', get_ad, name='get-ad'),
     path('get-ads/', ads_view, name='get-ads'),
     path('search/', search_view, name='search'),
     path('ordering/', AdListFilterView.as_view(), name='filter'),
+
+    path('create-ad/', create_ad, name='create-ad'),
 
 
 
