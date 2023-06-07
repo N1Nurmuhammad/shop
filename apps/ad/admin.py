67803b4c3a6cbff555531a492410d59db57f5b4d
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-06-07 12:03:32 +0500

    sub categories

--- apps/ad/admin.py
+++ apps/ad/admin.py
@@ -4,4 +4,6 @@ from apps.ad.models import *
 admin.site.register(AdModel)
 admin.site.register(FavAdModel)
 admin.site.register(AdPictureModel)
+admin.site.register(SubCategoriesModel)
+admin.site.register(CategoriesModel)
 
